3f05a51e0e7b2b61e9405c636a249f4b8a1add63
Mahade Hasan Forhad <mahadehasanforhad@gmail.com>
2024-02-21 22:45:35 +0600

    Search bar added.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
 
 //api key
 //51f6e886
 
-const API_URL = 'http://www.omdbapi.com?apikey=51f6e886';
-
+const API_URL = "http://www.omdbapi.com?apikey=51f6e886";
 
 const App = () => {
+  const searchMovies = async (tittle) => {
+    const response = await fetch(`${API_URL}&s=${tittle}`);
+    const data = await response.json();
 
+    console.log(data.Search);
+  };
 
-    const searchMovies = async (tittle) => {
-        const response = await fetch(`${API_URL}&s=${tittle}`);
-        const data = await response.json();
-        
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
 
-        console.log(data.Search);
-    }
+  return (
+    <div className="app">
+      <h1>MovieFlix</h1>
 
-    useEffect(() => {
-        searchMovies('spiderman');
-    }, []);
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
 
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
 
-    return (
-        <h1>App</h1>
-    );
-}
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
-
